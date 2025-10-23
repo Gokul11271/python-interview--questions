9683d3ea2c2cd5189c7e71b78ba8e5ee54d388aa
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-10-23 19:45:55 +0530

    Add functionality to fetch and display webpage content from a URL

--- special.py
+++ special.py
@@ -3,3 +3,10 @@
 #   if not (('a' <= i <= 'z') or ('A' <= i <= 'Z') or ('0' <= i <= '9')):
 #     print(i,end="")
 
+import urllib.request
+
+with urllib.request.urlopen("https://www.sparshdesigners.com/") as resp:
+	page_bytes = resp.read()
+	page_text = page_bytes.decode('utf-8', errors='replace')
+	print(resp.getcode())
+	print(page_text[:500])
