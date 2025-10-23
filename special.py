6fbd609c22276e21ae4096421a97520584ccb959
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-10-23 20:46:05 +0530

    vvvvvvvvvv

--- special.py
+++ special.py
@@ -10,3 +10,23 @@ with urllib.request.urlopen("https://www.sparshdesigners.com/") as resp:
 	page_text = page_bytes.decode('utf-8', errors='replace')
 	print(resp.getcode())
 	print(page_text[:500])
+	
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# load the 'tips' dataset from seaborn's public repo without requiring seaborn
+tips_url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv"
+tips = pd.read_csv(tips_url)
+
+# compute mean total_bill per day and plot with matplotlib
+mean_bills = tips.groupby("day")["total_bill"].mean()
+days = mean_bills.index.tolist()
+values = mean_bills.values.tolist()
+
+plt.bar(days, values)
+plt.xlabel("day")
+plt.ylabel("average total_bill")
+plt.title("Average total bill by day")
+plt.show()
+
