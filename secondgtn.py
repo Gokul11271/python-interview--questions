9c191e88ead997f5183686346d2fc984a41b64ca
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-10-26 20:59:25 +0530

    Refactor loop structure to enhance readability and output formatting

---
+++ secondgtn.py
@@ -0,0 +1,11 @@
+list = [1,2,3,4,5,6,7,8,9,10]
+
+for i in list:
+    
+    print(" ")
+    if i==1:
+       print(1)
+    for j in list:
+    #  print(j, end=" ")
+     if i>j:
+        print(i, end=" ")
