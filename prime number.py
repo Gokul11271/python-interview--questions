1c254dbe17024502f16629e6743ce108d5b8cf96
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-10-27 00:01:39 +0530

    p not

--- prime number.py
+++ prime number.py
@@ -1,14 +1,14 @@
-# num = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
 
-# if num > 1:
-#     for i in range(2, num):
-#         if num % i == 0:
-#             print("Not Prime")
-#             break
-#     else:
-#         print("Prime")
-# else:
-#     print("Not Prime")
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime")
+else:
+    print("Not Prime")
 
 # def isprime(num):
 #     for i in range(2, num-1):
