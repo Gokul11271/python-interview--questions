e928f1f6fed5c87c0af24b799aa79571641002d3
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-10-26 21:06:30 +0530

    2nd gtn

--- secondgtn.py
+++ secondgtn.py
@@ -1,11 +1,9 @@
 list = [1,2,3,4,5,6,7,8,9,10]
-
+maximun=(max(list))
 for i in list:
-    
-    print(" ")
-    if i==1:
-       print(1)
-    for j in list:
-    #  print(j, end=" ")
-     if i>j:
-        print(i, end=" ")
+    if i == maximun:
+        pass
+    else:
+        list2=[i]
+        
+print(list2)
