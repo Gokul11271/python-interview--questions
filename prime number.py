369e602ba1d00e22a8b91692cd4b05351f0e92ec
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-10-29 22:39:50 +0530

    Refactor prime number functions for clarity and consistency

--- prime number.py
+++ prime number.py
@@ -10,26 +10,26 @@ if num > 1:
 else:
     print("Not Prime")
 
-# def isprime(num):
-#     for i in range(2, num-1):
-#         if num % i == 0:
-#             return False      
-#     else:
-#         return True
+def isprime(num):
+    for i in range(2, num-1):
+        if num % i == 0:
+            return False      
+    else:
+        return True
         
 
 
-# def prime_n(n):
-#     for i in range(2, n+1):
-#         if isprime(i):
-#             print(i)
+def prime_n(n):
+    for i in range(2, n+1):
+        if isprime(i):
+            print(i)
 
 
-# n=10
-# for n in range(2,n):
-#         for i in range(2, n):
-#             if n % i ==0 :
-#                 print(n, "is  not prime")
-#                 break
-#         else:
-#               print(n, "is a prime") 
+n=10
+for n in range(2,n):
+        for i in range(2, n):
+            if n % i ==0 :
+                print(n, "is  not prime")
+                break
+        else:
+              print(n, "is a prime") 
