d7361efaf9c841fc5c72da94b6a3bb61724a1cfc
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-11-02 12:32:25 +0530

    class

---
+++ test_dir/class.py
@@ -0,0 +1,17 @@
+class hotel :
+    name="mess"
+    adr="bla bla "
+    mob=123456789
+    menu = {'a':100 ,'b':200,'c':300}
+
+    def order (self):
+       self.ordered_itemes = { }
+       lst={self.menu .keys()}
+       print(lst)
+
+    # genric,specific
+    def add(self,dish):
+    #   self.ordered_item[ ]
+      pass
+
+cus1=hotel()
