9b636942c215aaa0159a0b69cac7d539cbe72c97
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-11-05 00:10:42 +0530

    vv

--- DSA/random4.py
+++
@@ -1,9 +0,0 @@
-arr = [1,2,3,4,5,6,7,8,9,10]
-print(len(arr))
-target = 7
-
-for i in range(len(arr)):  # i = index (0..9)
-    if arr[i] == target:
-        print("value of the target:", arr[i])
-        print("index of the array:", i)
-fdv
