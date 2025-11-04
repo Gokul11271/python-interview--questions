a57555a4983c290c2187552bf4fbd1fd6e1f1a6f
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-11-04 23:51:29 +0530

    dfd

---
+++ DSA/index value.py
@@ -0,0 +1,10 @@
+
+arr=[1,2,3,4,5,6,7,8,9,10]
+print(len(arr))
+target=7
+for i in arr:
+    if arr[i] == target:
+        print("value of the target",i)
+        print("index of the array",arr[i])
+    else:
+        print("index of array value",i)
