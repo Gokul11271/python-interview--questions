a185a11fd6d3d89a61cbd5f709de6764f3c0b74e
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-11-05 12:16:46 +0530

    array

---
+++ DATA_SA/Array/Sum and average of all elements.py
@@ -0,0 +1,10 @@
+# Output: total sum and average value.
+
+arr=[1,2,3,4,5,6]
+sum=0
+avg=0
+for i in arr:
+    sum+=i
+    avg =sum%len(arr)
+
+print(f"sum of values {sum }, average of the array: {avg}")
