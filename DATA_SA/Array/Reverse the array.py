0cf3cf5e127b54503af07259a0685f3168c5b733
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-11-05 13:45:36 +0530

    aaaa

---
+++ DATA_SA/Array/Reverse the array.py
@@ -0,0 +1,14 @@
+# Reverse the array
+
+# Without using built-in reverse methods.
+
+# 👉 Use two-pointer swapping or slicing.
+
+
+arr=[1,2,3,4,5,6]
+# print(arr[::-1])
+
+arr1=[ ]
+for i in range(len(arr) ):
+    arr1.append(i)
+print(arr1)
