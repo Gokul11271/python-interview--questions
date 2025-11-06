529ccbc337057234f0fca575b603c9b837e3b8e8
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-11-06 22:24:34 +0530

    lll

---
+++ DATA_SA/Array/⚙️ LEVEL 2 — Searching & Pair Problems.py
@@ -0,0 +1,29 @@
+# ⚙️ LEVEL 2 — Searching & Pair Problems
+
+# Learn nested loops, conditions, and two-pointer logic.
+
+# Linear search
+
+# Return index if found else -1.
+
+# Find all pairs with a given sum
+
+# Input: [2,4,3,5,7,8,9], target=10
+
+# Output: (2,8), (3,7), (4,6) (if exists)
+
+# 👉 Practice your current concept.
+
+# Find duplicates in an array
+
+# Input: [1,2,3,2,1,4]
+
+# Output: 1,2
+
+# Find second largest element
+
+# Without sorting — O(n) time.
+
+# Check if array is sorted (ascending)
+
+# Output: True / False
