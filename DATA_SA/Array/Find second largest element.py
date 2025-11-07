26be4cd467df3078337eaafd159ad1b6f8e25a37
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-11-07 23:58:33 +0530

    dd

--- DATA_SA/Array/Find second largest element.py
+++ DATA_SA/Array/Find second largest element.py
@@ -1,3 +1,13 @@
 # Find second largest element
 
 # Without sorting — O(n) time.
+
+
+
+
+arr1 = [10,20,30,40,50,60,70,80,90,100]
+target=30
+for i in range(len(arr1)):
+    for j in range(i+1,len(arr1)):
+        if i+j==target:
+            print(arr1[i],arr1[j])
