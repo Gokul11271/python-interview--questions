c859a9e25b49456854ee44d4730225e33b17b30a
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-11-24 20:15:56 +0530

    arr gtn

--- DSA/max_arr.py
+++ DSA/max_arr.py
@@ -1,6 +1,11 @@
 arr = [10, 20, 5, 90, 56]
-
+count=0
 maz=arr[0]
-for i in arr:
-    if arr > i:
-        a
+
+for i  in arr:
+    count+=1
+    print(count,"max[]",maz,"less than",arr,i )
+    if maz<=i:
+        maz=i
+       
+print(maz,"*")
