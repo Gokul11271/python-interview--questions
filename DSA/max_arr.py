21bbb89ae2ffd76a5b1e735c7dbea875430edef9
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-11-24 23:09:48 +0530

    maximum in array

--- DSA/max_arr.py
+++ DSA/max_arr.py
@@ -4,8 +4,21 @@ maz=arr[0]
 
 for i  in arr:
     count+=1
-    print(count,"max[]",maz,"less than",arr,i )
+    # print(count,"max[]",maz,"less than",arr,i )
     if maz<=i:
         maz=i
        
 print(maz,"*")
+max=0
+for i in range(len(arr)):
+    for j in range((i+1),len(arr)) :
+        # print(arr[i],arr[j])
+        if arr[i]>= arr[j]:
+            max=arr[i]
+print(max)
+
+maximum=arr[0]
+for i in arr:
+    if i > maximum:
+        maximum = i
+print(maximum)
