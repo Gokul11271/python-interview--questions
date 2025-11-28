cd44f762f986930c1eea492ab74e08295361deff
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-11-28 18:17:32 +0530

    ..

---
+++ DSA/day1.py
@@ -0,0 +1,20 @@
+a=25
+if(a<18):
+    print("Elgible")
+
+def add(a, b):   # a, b → parameters
+    
+    print(a+b)
+add(10,20)
+
+
+def sdd(a=10, b=10):   # a, b → parameters
+    
+    print(a+b)
+sdd()
+
+
+
+a=11
+for i in range(a):
+    print(i,"2",i*2)
