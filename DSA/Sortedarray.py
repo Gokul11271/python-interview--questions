e65cb2909b9677803584d887efbc6593306268c4
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-12-02 18:34:37 +0530

    assss and desc

---
+++ DSA/Sortedarray.py
@@ -0,0 +1,8 @@
+list=[10,22,34,500,67,89,18,23]
+print(list)
+print()
+for i in range(len(list)):
+    for j in range(i+1,len(list)):
+        if list[i]>list[j]:
+            list[i],list[j]=list[j],list[i]
+print(list)
