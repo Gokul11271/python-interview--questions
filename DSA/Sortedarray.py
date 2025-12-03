913d5ec2ad775f4a4f50a1067ffccb32c701a2f1
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-12-03 16:09:41 +0530

    ..

--- DSA/Sortedarray.py
+++ DSA/Sortedarray.py
@@ -1,8 +1,12 @@
-list=[10,22,34,500,67,89,18,23]
+list=[5,4,3,1,2]
 print(list)
 print()
 for i in range(len(list)):
     for j in range(i+1,len(list)):
+        print(list[i],list[j])
         if list[i]>list[j]:
             list[i],list[j]=list[j],list[i]
+            print(list[i],list[j])
+    print(list,"the is the inner loop list")
+    
 print(list) 
