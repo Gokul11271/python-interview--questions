5f4a29829034ef4f3a0dbbb761b928c07e46568e
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-12-04 14:40:27 +0530

    ,,,

--- DSA/Add two value in a list.py
+++ DSA/Add two value in a list.py
@@ -1,20 +1,16 @@
 # Input: l1 = [2,4,3], l2 = [5,6,4]
 # Output: [7,0,8]
 
-l1 = [2,4,3]
+l1 = [23,43,33]
 l2 = [5,6,4]
+
 # sum=0
 # for i in range(len(l1)):
 #     for j in range(len(l2)):
 #     #  print(l1[i],"+",l2[j],"=",l1[i]+l2[j])
 #      sum=l1[i]+l2[j]
 #      print(sum)
+
 for i in l1:
-   
-    for j in l2:
-        print(i,j)
-        print(i+j)
-        i+=1
-        
-        break
-       
+    carry=i/10   
+    print(int(carry))
