6c2363f9878be195627b7b6aa771b0319fcff11c
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-12-05 22:22:16 +0530

    ,,,,

--- DSA/Add two value in a list.py
+++ DSA/Add two value in a list.py
@@ -1,7 +1,7 @@
 # Input: l1 = [2,4,3], l2 = [5,6,4]
 # Output: [7,0,8]
 
-# l1 = [23,43,33]
+# l1 = [2,4,3]
 # l2 = [5,6,4]
 
 # sum=0
@@ -11,9 +11,44 @@
 #      sum=l1[i]+l2[j]
 #      print(sum)
 
-l1 = [2,4,3]
-l2 = [5,6,4]
-# Output: [7,0,8]
-for i in l1:
-    carry=i%10   
-    print(carry)
+# l1 = [1,2,3]
+# l2 = [4,5,10]
+# carry=0
+# count=0
+# for i in range(len(l1) -1,-1,-1):
+#  for j in range(len(l2)-1,-1,-1):
+#   sum=l1[i]+l2[i]+carry
+#   print(carry,"💖")
+  
+# #   print(count,"=",sum)
+#   if sum > 9:
+#    carry=sum//10
+#    sum=sum%10
+# #    print(carry,"carry")
+#    print(sum,"sum + carry")
+#    print(carry)
+#   else:
+#    print(sum,"sum 🔥",end=" ")
+#    carry=0
+
+
+
+l1 = [1,2,3]
+l2 = [4,5,10]
+carry=0
+
+for i in range(len(l1) -1,-1,-1):
+#  for j in range(len(l2)-1,-1,-1):
+  sum=l1[i]+l2[i]+carry
+#   print(carry,"💖")
+  
+#   print(count,"=",sum)
+  if sum > 9:
+   carry=sum//10 #13 1
+   sum=sum%10 # 3
+#    print(carry,"carry")
+   print(sum,end=" ")
+#    print(carry)
+  else:
+   print(sum,end=" ")
+   carry=0
