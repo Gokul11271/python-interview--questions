b07f28e539bb6ab8fb876a03bacad9c5179c9381
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2026-02-19 22:49:52 +0530

    ..

---
+++ DATA_SA/Array/pratice.py
@@ -0,0 +1,12 @@
+list=[8,3,1,6,2]
+a=0
+for i in range(len(list)):
+    # print(i,list[i])
+    for j in range(len(list)):
+        
+        if list[i]>list[j]:
+            print(j," ",list[i],">",list[j])
+            
+      
+            
+            
