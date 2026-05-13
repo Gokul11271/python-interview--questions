e653ce5ff5f2afe15a9301f82cde48cd869834c8
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2026-05-13 12:53:48 +0530

    ..

---
+++ May/2026 Data structure/level 0 functions.py
@@ -0,0 +1,28 @@
+"""Function is a block of resuable code which will exacute untill it is called by their own function name """
+"in python we use the def as keword for function "
+
+
+"simple function"
+"syntax   def funtionname ():"
+"                     condition or statement "
+"                     which execture at lest one time "
+
+def sum():
+    t = 10 + 340
+    print(t)
+
+sum()
+
+
+def total(a,b):
+    tt=a+b
+    print(tt)
+
+total(10,100)
+
+def square(n):
+    return n * n
+
+result = square(4)
+
+print(result)
