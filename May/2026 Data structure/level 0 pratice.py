b98b6159b5a6a19d56b48976523266a9e1b2c93c
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2026-05-16 12:39:51 +0530

    factorial

--- May/2026 Data structure/level 0 pratice.py
+++ May/2026 Data structure/level 0 pratice.py
@@ -31,11 +31,21 @@ a,b,c=4,9,2
 #     print(c)
 
 
-n=5
+# n=5
 # for i in range(n,0,-1):
 #     print(i)
 
-sum =0
-for i in range(1,n+1):
-    sum += i
-print(sum)
+# sum =0
+# for i in range(1,n+1):
+#     sum += i
+# print(sum)
+
+
+
+# factorial
+# n=5
+# a=1
+
+# for i in range(1,n+1):
+#     a *= i
+# print(a)
