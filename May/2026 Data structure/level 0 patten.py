bffd14bb339cac56b2b081bb12d968d504c7bfd9
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2026-05-19 20:18:04 +0530

    reverse star patten

--- May/2026 Data structure/level 0 patten.py
+++ May/2026 Data structure/level 0 patten.py
@@ -16,5 +16,15 @@ matrix 4 x 4 i= 4 j =4
 #   for j in range(ip):
 #     print("*",end=" ")
 
+# ip =4
 
+# for i in range(ip):
+#     print( )
+#     for j in range(i+1):
+#         print("*",end=" ")
     
+ip=4
+for i in range(ip):
+    print( )
+    for j in range(ip-i):
+        print("*",end= " ")
