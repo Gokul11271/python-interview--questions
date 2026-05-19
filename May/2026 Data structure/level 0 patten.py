bdb02e1baed762b0f07a09154cdbe765c935567d
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2026-05-19 20:26:34 +0530

    pyramid patten

--- May/2026 Data structure/level 0 patten.py
+++ May/2026 Data structure/level 0 patten.py
@@ -29,8 +29,14 @@ matrix 4 x 4 i= 4 j =4
 #     for j in range(ip-i):
 #         print("*",end= " ")
 
+# ip=5
+# for i in range(ip):
+#     print( )
+#     for j in range(i+1):
+#         print(j+1,end=" ")
+
 ip=5
 for i in range(ip):
     print( )
     for j in range(i+1):
-        print(j+1,end=" ")
+     print("*",end=" ")
