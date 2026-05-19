501992932d222ad7f9c022c676babd0570faec98
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2026-05-19 20:23:30 +0530

    number patten

--- May/2026 Data structure/level 0 patten.py
+++ May/2026 Data structure/level 0 patten.py
@@ -23,8 +23,14 @@ matrix 4 x 4 i= 4 j =4
 #     for j in range(i+1):
 #         print("*",end=" ")
     
-ip=4
+# ip=4
+# for i in range(ip):
+#     print( )
+#     for j in range(ip-i):
+#         print("*",end= " ")
+
+ip=5
 for i in range(ip):
     print( )
-    for j in range(ip-i):
-        print("*",end= " ")
+    for j in range(i+1):
+        print(j+1,end=" ")
