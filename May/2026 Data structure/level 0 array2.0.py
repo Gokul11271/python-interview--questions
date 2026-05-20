6428939220623e88eb23a5f42f5e1d9313fc7829
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2026-05-20 19:05:20 +0530

    sum in array

--- May/2026 Data structure/level 0 array2.0.py
+++ May/2026 Data structure/level 0 array2.0.py
@@ -2,6 +2,13 @@
 
 """
 
-arr=[1,2,3,4]
-for i in arr:
-    print(i)
+# arr=[1,2,3,4]
+# for i in arr:
+#     print(i)
+
+# """sum"""
+# arr=[1,2,3]
+# sum=0
+# for i in arr:
+#     sum+=i
+# print(sum)
