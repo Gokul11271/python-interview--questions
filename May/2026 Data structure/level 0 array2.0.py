56b0fb0e0fde7202952c616a69db5004cddf21c2
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2026-05-20 19:15:56 +0530

    gt

--- May/2026 Data structure/level 0 array2.0.py
+++ May/2026 Data structure/level 0 array2.0.py
@@ -12,3 +12,16 @@
 # for i in arr:
 #     sum+=i
 # print(sum)
+
+"""Problem 25 — Largest Element
+
+Input:
+
+[4,8,2,9] """
+
+ip=[4,8,2,9,]
+for i in range(len(ip)):
+    for j in range(len(ip)):
+        if ip[i]>ip[j]:
+             largest=ip[i]
+print(largest)
