19428e67424849c0e317b518f48e56a81f926b68
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2026-05-25 21:02:15 +0530

    vv

--- May/2026 Data structure/level 0 logic_building.py
+++ May/2026 Data structure/level 0 logic_building.py
@@ -12,17 +12,38 @@ ip=5
 #         continue
 #     print(answer)
 
-ip=5
-current=0
-previous=0
-for i in range(5):
-    if i==0 or i==1:
-        print(i) 
-        previous =i
-        if i ==1:
-         print(previous)
-previous+=current+previous
-current=previous
+# ip=5
+# current=0
+# previous=0
+# for i in range(5):
+#     if i==0 or i==1:
+#         print(i) 
+#         previous =i
+#         print(previous,"prrrr")
+#         print(current,"crrr")
+#     elif i>1:
+                     
+#      previous+=current+previous
+#      current=previous
 
-print(previous)
-print("current",current)
+#      print(previous)
+#      print("current",current)
+
+
+#     |c  |n  | s | O |
+# 
+# | 1 | 0 | 1 | 1 | 0 |
+# | 2 | 1 | 1 | 2 | 1 |
+# | 3 | 1 | 2 | 3 | 1 |
+# | 4 | 2 | 3 | 5 | 2 |
+# | 5 | 3 | 5 | 8 | 3 |
+# | 6 | 5 | 8 | 13 | 5 |
+# | 7 | 8 | 13 | 21 | 8 |
+ip=5
+c,n=0,0
+for i in range(ip):
+    if i == 0 or i ==1:
+        n=i
+        print(i)
+    elif i > 1:
+         
