7a8450d324e27cb83253e900998f4dd750be5d90
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2026-06-16 14:13:09 +0530

    learning day 2

---
+++ May/2026 Data structure/level 0 Sorting Basics.py
@@ -0,0 +1,27 @@
+"""SECTION 10 — Sorting Basics
+Problem 37 — Bubble Sort
+
+Input:
+
+[5,2,1,4]
+
+Output:
+
+[1,2,4,5]"""
+
+# ip=[5,2,1,4]
+
+# for i in range(len(ip)):
+#     print()
+#     for j in range(1,len(ip)):
+#         if ip[i]<ip[j]:
+#             print(ip[i],ip[j])
+#             ip[i]=ip[j]
+#             continue
+
+# print(ip)
+
+n=10
+for i in range(n):
+  pass
+print(i)
---
+++ May/2026 Data structure/level 0 search.py
@@ -0,0 +1,8 @@
+"""Array = [2,5,7,9]
+Target = 7"""
+
+Array = [2,5,7,9]
+Target = 7
+for i in Array:
+    if i == Target:
+        print("found")
