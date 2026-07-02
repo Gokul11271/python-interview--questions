dde76ad67be8804cf691defef767355831aa8f2c
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2026-07-02 15:22:27 +0530

    ,,

--- May/2026 Data structure/level 0 second largest .py
+++ May/2026 Data structure/level 0 second largest .py
@@ -1,3 +1,28 @@
 # [5,3,8,2,9]
 
 # Find Second Largest
+
+
+a = 10
+b = 20
+if a > b:
+    print(f"{a} is greater than {b}")
+elif a < b:
+    print(f"{b} is greater than {a}")
+else:
+    print(f"{a} and {b} are equal")
+    
+
+if a <b:
+    print(f"{a} is Smaller than {b}")
+elif a > b:
+    print(f"{b} is Smaller than {a}")
+else:
+    print(f"{a} and {b} are equal")
+
+a=10
+b=[]
+for i  in range(a):
+ b.insert(0,i)
+ print(b)
+ 
