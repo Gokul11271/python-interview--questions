c8055388c1f6a4971cb98df4b3dc0b389ea6da1f
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2026-07-20 15:24:45 +0530

    cc

--- May/2026 Data structure/oops.py
+++ May/2026 Data structure/oops.py
@@ -8,3 +8,16 @@ class car :
  pass
 c1=car
 print("executed")
+
+"""Problem 2
+
+Complete the following:
+
+Create a class called Book.
+Create two objects named book1 and book2."""  
+
+class Book :
+ pass
+book1=Book()
+book2=Book()
+print("executed")
